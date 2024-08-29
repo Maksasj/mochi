9216d0de0d1fa976e6016ed42e0d261a2c97e7ee
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-08-29 19:37:07 +0300

    to the moon !

---
+++ Mochi.Service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mochi.Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
