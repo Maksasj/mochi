016c99a4bda02fb194d73be80ad549d5fc57b64f
Maksasj <radioboos@gmail.com>
2024-09-20 11:45:26 +0300

    Utils controller

---
+++ Mochi.Service/Controllers/UtilsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mochi.Service.Controllers
+{
+    public class UtilsController : Controller
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Health")]
+        public async Task<IActionResult> GetHealth()
+        {
+            return Ok();
+        }
+    }
+}
